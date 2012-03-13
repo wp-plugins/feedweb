ca0f528daf692a57afd14e2f60ac03c65b96eb67
Feedweb <Feedweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-13 05:58:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/feedweb/tags/1.0.2@518290 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ feedweb_settings.php
@@ -0,0 +1,52 @@
+<?php
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(dirname(dirname(dirname(__FILE__)))) . '/');
+
+require_once( ABSPATH.'wp-load.php');
+require_once( 'feedweb_util.php');
+
+if (!current_user_can('manage_options'))
+	wp_die(__("You are not allowed to be here"));
+else
+	UpdateSettings();
+
+$error_message = "";
+
+function UpdateSettings()
+{
+	$data = array();
+	$data["language"] = $_POST["FeedwebLanguage"];
+	$data["mp_widgets"] = $_POST["FeedwebMPWidgets"];
+	$data["widget_width"] = $_POST["WidgetWidthEdit"];
+	
+	if (SetFeedwebOptions($data))
+		$error_message = "";
+	else
+		$error_message = "Failed to update settings";
+}
+
+function GetErrorMessage()
+{
+	global $error_message;
+	echo $error_message;
+}
+?>
+
+<html>
+<head>
+	<script language="javascript" type="text/javascript">
+		function OnInit()
+		{
+			var error = "<?php echo GetErrorMessage();?>";
+			if (error != "")
+				window.alert (error);
+
+			var href = "<?php echo $_POST["_wp_http_referer"]?>";
+			window.location.href = href;
+		}
+	</script>
+</head>
+<body onload="OnInit()">
+</body>
+</html> 

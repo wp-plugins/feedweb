9d01c0f6fff3aff6a4b4e3a4f6addb0264b37b6d
Feedweb <Feedweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-22 12:35:31 +0000

    git-svn-id: https://plugins.svn.wordpress.org/feedweb/tags/1.0.7@522180 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ widget_remove.php
@@ -0,0 +1,76 @@
+<?php
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(dirname(dirname(dirname(__FILE__)))) . '/');
+
+require_once( ABSPATH.'wp-load.php');
+require_once( ABSPATH.'wp-admin/includes/template.php');
+
+function GetAction()
+{
+	echo "widget_commit.php?wp_post_id=".$_GET["wp_post_id"]."&feedweb_cmd=DEL";
+}
+
+function GetPostTitle()
+{
+	$id = intval($_GET["wp_post_id"]);
+	echo get_the_title($id);
+}
+
+
+?>
+
+<html>
+	<head>
+		<script type="text/javascript">
+			function OnCancel() 
+			{ 
+				window.parent.tb_remove(); 
+			} 
+		
+			function OnLoad()
+			{
+		    }
+
+			function OnSubmitForm()
+			{
+				document.forms[0].action = "<?php GetAction()?>";
+				document.forms[0].method = "post";
+				return true;
+			}
+		</script>
+
+		<link rel='stylesheet' href='<?php echo get_bloginfo('url') ?>/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load=wp-admin' type='text/css' media='all' />
+		<link rel='stylesheet' id='thickbox-css'  href='<?php echo get_bloginfo('url') ?>/wp-includes/js/thickbox/thickbox.css' type='text/css' media='all' />
+		<link rel='stylesheet' id='colors-css'  href='<?php echo get_bloginfo('url') ?>/wp-admin/css/colors-fresh.css' type='text/css' media='all' />
+		
+	</head>
+	<body onload="OnLoad()" style="margin: 0px;">
+		<div id="RevoveWidgetDialog" >
+		 	<form id="RemoveWidgetDialogForm" onsubmit="return OnSubmitForm();">
+		 		<table>
+		 			<tbody>
+		 				<tr style="height:5px;">
+		 					<td style="width: 10px;"/>
+ 						 	<td style="width: 555px;"/>
+ 						 	<td style="width: 10px;"/>
+		 				</tr>
+		 				<tr style="height:50px;">
+		 					<td/>
+		 					<td align="center">
+ 								<span id='PromptLabel'><b>The widget in the post <i>'<?php echo GetPostTitle()?>'</i> will be removed</b></span>
+		 					</td>
+		 					<td/>
+		 				</tr>
+		 				<tr valign="bottom">
+		 					<td/>
+		 					<td align="center">
+								<?php echo get_submit_button('Remove Widget', 'primary', 'submit', false, "style='width: 100%;'") ?>
+		 					</td>
+		 					<td/>
+		 				</tr>
+		 			</tbody>
+		 		</table>
+		  	</form>
+		</div>
+	</body>
+</html>

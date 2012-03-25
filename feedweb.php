1a2c8b966e74cd488463411966edc5e1d3a23b2f
Feedweb <Feedweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-25 18:50:35 +0000

    git-svn-id: https://plugins.svn.wordpress.org/feedweb/trunk@523463 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- feedweb.php
+++ feedweb.php
@@ -4,7 +4,7 @@ Plugin Name: Feedweb
 Plugin URI: http://wordpress.org/extend/plugins/feedweb/
 Description: Expose your blog to the Feedweb reader's community, promote your views, get a comprehensive and detailed feedback from your readers.
 Author: Feedweb
-Version: 1.1.1
+Version: 1.1.2
 Author URI: http://feedweb.net
 */
 

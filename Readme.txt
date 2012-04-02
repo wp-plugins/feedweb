00c50c3d56269298afd09dfca52b263b98e40cf1
Feedweb <Feedweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-02 05:18:03 +0000

    git-svn-id: https://plugins.svn.wordpress.org/feedweb/tags/1.1.8@525942 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,59 @@
+=== Feedweb ===
+Contributors: Feedweb
+Tags: blog, rating, network, feedback, public, opinion, research, flash, widget, media, press, news, politics, society, social, question, answer, survey, poll, stats, discussion, controversy, cooperation, view, comments, statistics
+Requires at least: 3.0
+Tested up to: 3.3.1
+Stable Tag: 1.1.8
+
+Expose your blog to the Feedweb reader's community. Promote your views. Get a comprehensive and detailed feedback from your readers.
+
+== Description ==
+
+The Blog Rating Plugin is a simple and easy way to promote your views, improve your blog and to contribute to the Feedweb Research Network. The plugin helps you expose your blog to the Feedweb Readers' Community, and participate in various public opinion research projects.
+After activating the plugin you can freely insert customized blog rating widgets in your selected posts, choose questions you want to ask your readers, and receive a comprehensive, detailed and precise feedback from them.
+The Blog Rating Widget is an attractive animated visual element that will improve your ratings! It is built using a cutting-edge Flash technology with the highest privacy and security standards, and enterprise-level quality.
+Requires WordPress 3.0 and PHP 5.
+
+
+== Installation ==
+
+1. Upload the Feedweb plugin to your blog and activate it. 
+2. Choose your preffered language, widget width and other options in the Feedweb Settings page. 
+3. In the Posts table choose a post and click 'Insert' button. 
+4. Choose the questions you want to ask your readers and commit the widget. The number of widget-powered posts in your blog is unlimited.
+5. View your post and see the widget in the bottom of the post's content.
+
+== Changelog ==
+
+= 1.1.7 =
+* Text domain bug fix.
+
+= 1.1.6 =
+* Recommended width correction.
+
+= 1.1.5 =
+* 'Settings' link correction.
+
+= 1.1.4 =
+* 'Copyright notice' option.
+
+= 1.1.1 =
+* Copyright notice.
+
+= 1.1 =
+* New version of the flash widget.
+
+= 1.0.8 =
+* Fix for PHP < 5.3.0.
+
+= 1.0.7 =
+* Post age limit.
+
+= 1.0.6 =
+* Widget: Hebrew language support.
+
+= 1.0.5 =
+* Widget: Russian language support.
+
+= 1.0.4 =
+* 'Delay results' option.
---
+++ widget_commit.php
@@ -0,0 +1,131 @@
+<?php
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(dirname(dirname(dirname(__FILE__)))) . '/');
+
+require_once( ABSPATH.'wp-load.php');
+require_once( 'feedweb_util.php');
+
+if (!current_user_can('manage_options'))
+	wp_die(__("You are not allowed to be here"));
+
+$alert = "";
+$cmd = $_GET["feedweb_cmd"];
+$id = intval($_GET["wp_post_id"]);
+
+if ($cmd == "DEL")
+	RemovePac($id);
+else
+	InsertWidget($id);
+
+function InsertWidget($id)
+{
+	$pac = CreatePageWidget($id);
+	if ($pac == null)
+		return;
+	
+	global $alert;
+	if (InsertPac($pac, $id) == false)
+		$alert = __("Wordpress cannot insert your widget", "FWTD");
+}
+
+function CreatePageWidget($id)
+{
+	global $alert;
+	$data = GetFeedwebOptions();
+	
+	try
+	{
+		$query = GetFeedwebUrl()."FBanner.aspx?action=cpw&lang=".$data["language"];
+		
+		if ($_POST["WidgetQuestionsData"] != "")
+		{
+			$questions = explode ( "|", $_POST["WidgetQuestionsData"]);
+			$count = count($questions);
+		
+			for ($index = 0; $index < $count; $index++)
+			{
+				$question = $questions[$index];
+				if ($question == "")
+					break;
+				
+				if (substr($question, 0, 1) == "@")
+				{
+					$question = substr($question, 1);
+					$question = PrepareParam($question);
+				}
+				else
+					$question = "{".$question."}";
+				$query = $query."&q".$index."=".$question;
+			}
+			$query = $query."&qn=".$count;
+		}
+		
+		if ($data["delay"] != "0")
+			$query = $query."&delay=".$data["delay"];
+		
+		$code = GetPostCode($id);
+		if ($code == null)
+		{
+			$alert = __("Error in the Post data", "FWTD");
+			return null;
+		}
+		$query = $query."&code=".$code;
+				
+		$response = wp_remote_get ($query, array('timeout' => 60));
+		if (is_wp_error ($response))
+		{
+			$alert = __("Cannot connect Feedweb server", "FWTD");
+			return null;
+		}
+		
+		$dom = new DOMDocument;
+		if ($dom->loadXML($response['body']) == true)
+		{
+			$el = $dom->documentElement;
+			if ($el->tagName == "BANNER")
+			{
+				$pac = $el->getAttribute("pac");
+				if ($pac != "")
+					return $pac;
+				
+				$alert = $el->getAttribute("error");
+				if ($alert == "")
+					$alert = __("Unknown server error", "FWTD");
+				return null;
+			}
+		}
+		$alert = __("Feedweb service cannot create a widget", "FWTD");
+	}
+	catch (Exception $e)
+	{
+		$alert = $e->getMessage();
+	}
+	return null;
+}
+
+
+function GetAlertText()
+{
+	global $alert;
+	echo $alert;
+}
+?>
+
+<html>
+	<head>
+		<script language="javascript" type="text/javascript">
+			function OnInit()
+			{
+				var text = "<?php GetAlertText() ?>";
+				if (text != "")
+					window.alert(text);
+				
+				window.parent.tb_remove();
+				window.parent.location.href = window.parent.location.href;
+			}
+		</script>
+	</head>
+	<body onload="OnInit()">
+	</body>
+</html> 

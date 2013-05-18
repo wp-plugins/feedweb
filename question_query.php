ace2760f3a48756142cb8531b79b9f9a36da4341
Feedweb <Feedweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-18 18:05:48 +0000

    Version 2.0.4
    git-svn-id: https://plugins.svn.wordpress.org/feedweb/tags/2.0.4@715028 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ question_query.php
@@ -0,0 +1,37 @@
+﻿<?php
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(dirname(dirname(dirname(__FILE__)))) . '/');
+
+require_once( ABSPATH.'wp-load.php');
+require_once( 'feedweb_util.php');
+
+if (!current_user_can('manage_options'))
+	wp_die(__("You are not allowed to be here"));
+else
+	GetQuestionList();
+ 
+function GetQuestionList()
+{
+	// Get Language
+	$lang = $_GET["lang"];
+	$site = PrepareParam(site_url());
+	$url = GetFeedwebUrl()."FBanner.aspx?action=gql&lang=$lang&site=$site";
+	
+	$response = wp_remote_get ($url, array('timeout' => 60));
+	if (is_wp_error ($response))
+		return;
+		
+	$dom = new DOMDocument;
+	if ($dom->loadXML($response['body']) == true)
+		if ($dom->documentElement->tagName == "BANNER")
+		{
+			$questions = ReadQuestionList($dom->documentElement);
+			if ($questions == null)
+				return;
+				
+			foreach ($questions as $id => $text)
+				echo "<option value='$id'>$text</option>";
+		}
+}
+?>

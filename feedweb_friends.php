52607438505c61aab9255f3abc852e440f87e60d
Feedweb <Feedweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-16 22:07:25 +0000

    Version 2.1.6
    git-svn-id: https://plugins.svn.wordpress.org/feedweb/tags/2.1.6@741961 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ feedweb_friends.php
@@ -0,0 +1,38 @@
+<style type="text/css">
+	.FriendPromotionDiv
+	{
+		position: relative;
+	}
+	
+	.FriendPromotionDiv a
+	{
+		position: absolute;
+	}
+	
+	.FriendPromotionDiv a img
+	{
+		width: 500px;
+	}
+	
+	.FriendPromotionDiv a span
+	{
+		position: absolute;
+		font-weight: bold;
+		display: block;
+		width: 500px;
+		top: 180px;
+	}
+</style>
+
+<div class="wrap">
+	<div id="icon-plugins" class="icon32"><br /></div>
+	<h2><?php _e("Our Friends - The Plugins We Recommend", "FWTD");?></h2><br/>
+	
+	<div class="FriendPromotionDiv">
+		<a href="http://wordpress.org/plugins/the-casengo-chat-widget/" target="_blank" style="top: 0px; left: 0px;">
+			<img src="<?php echo plugin_dir_url(__FILE__)?>/images/Livechat-Casengo.jpg"/>
+			<br/>
+			<span>Casengo is Customer Support Software (Live Chat, Email, Social Media) from the Cloud, First User is Always Free!</span>
+		</a>
+	</div>
+</div>

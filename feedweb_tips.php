42b1f251dae1c20c6d40bd917145cbcfbb5088f5
Feedweb <Feedweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-08 21:34:16 +0000

    Version 2.3.1
    git-svn-id: https://plugins.svn.wordpress.org/feedweb/tags/2.3.1@784864 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ feedweb_tips.php
@@ -0,0 +1,44 @@
+<style>
+.SitePromotionDiv
+{
+	position: relative;
+}
+
+.SitePromotionDiv div,
+.SitePromotionDiv a
+{
+	position: absolute;
+}
+
+.SitePromotionDiv .LogoImage
+{
+	width: 400px;
+}
+
+.SitePromotionDiv a span
+{
+	position: absolute;
+	font-weight: bold;
+	display: block;
+	width: 400px;
+	top: 330px;
+}
+
+</style>
+
+<div class="wrap">
+	<div id="icon-edit-pages" class="icon32"><br /></div>
+	<h2><?php _e("Web Reviews, Blogging Tips & Useful Info", "FWTD");?></h2><br/>
+	
+	<div class="SitePromotionDiv">
+		<div style="top: 0px; left: 0px;">
+			<a href="http://www.bloggerdoc.com/" target="_blank" style="top: 0px; left: 0px;">
+				<img class="LogoImage" src="<?php echo plugin_dir_url(__FILE__)?>/images/BloggerDoc.jpg"/>
+			</a>
+			<a href="http://www.bloggerdoc.com/" target="_blank" style="top: 0px; left: 0px;">
+				<span>Make your Blog or Website go viral by learning the best techniques. Monetize your blog online and become an Internet Marketing Guru.</span>
+			</a>
+		</div>
+	</div>
+</div>
+
